fa11db653f3e90f22fac85d3b039073201d8af4f
James Pouliot <itdbrookline@gmail.com>
2025-01-10 12:39:30 -0500

    Exercises 1.1 and 1.2 done.

--- part 01/course-information-app/src/App.jsx
+++ part 01/course-information-app/src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = () => {
+	const course = "Half Stack application development";
+	const part1 = "Fundamentals of React";
+	const exercises1 = 10;
+	const part2 = "Using props to pass data";
+	const exercises2 = 7;
+	const part3 = "State of a component";
+	const exercises3 = 14;
 
-function App() {
-  const [count, setCount] = useState(0)
+	return (
+		<>
+			<Header course={course} />
+			<Content
+				part1={part1}
+				part2={part2}
+				part3={part3}
+				exercises1={exercises1}
+				exercises2={exercises2}
+				exercises3={exercises3}
+			/>
+			<Total amount={exercises1 + exercises2 + exercises3} />
+		</>
+	);
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const Header = (props) => {
+	return (
+		<>
+			<h1>{props.course}</h1>
+		</>
+	);
+};
 
-export default App
+const Content = (props) => {
+	return (
+		<>
+			<Part partNumber={props.part1} exerciseAmount={props.exercises1} />
+			<Part partNumber={props.part2} exerciseAmount={props.exercises2} />
+			<Part partNumber={props.part3} exerciseAmount={props.exercises3} />
+		</>
+	);
+};
+
+const Part = (props) => {
+	return (
+		<>
+			<p>
+				{props.partNumber} {props.exerciseAmount}
+			</p>
+		</>
+	);
+};
+
+const Total = (props) => {
+	return (
+		<>
+			<p>Number of exercises {props.amount}</p>
+		</>
+	);
+};
+
+export default App;
