f46a6b7efcaa32bf362c3aff970104b320066289
James Pouliot <itdbrookline@gmail.com>
2025-01-16 16:02:29 -0500

    finished 2.6

---
+++ part 02/phonebook/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+const Number = (props) => {
+	console.log("inside Number, person is:", props.person);
+	return <div>{props.person.name}</div>;
+};
+
+const App = () => {
+	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+	const [newName, setNewName] = useState("");
+
+	const addName = (event) => {
+		event.preventDefault();
+		console.log("adding name: ", newName);
+		const newNameObject = {
+			name: newName,
+		};
+		console.log("new name object: ", newNameObject);
+		const updatedPersons = persons.concat(newNameObject);
+		setPersons(updatedPersons);
+		console.log(updatedPersons);
+		// updatedPersons = persons.concat(person)
+	};
+
+	const handleNewNameChange = (event) => {
+		console.log(event.target.value);
+		setNewName(event.target.value);
+	};
+
+	return (
+		<div>
+			<h2>Phonebook</h2>
+			<form onSubmit={addName}>
+				<div>
+					name: <input value={newName} onChange={handleNewNameChange} />
+				</div>
+				<div>
+					<button type='submit'>add</button>
+				</div>
+			</form>
+			<h2>Numbers</h2>
+			{persons.map((person) => (
+				<Number key={person.name} person={person} />
+			))}
+			<div>debug: {newName}</div>
+		</div>
+	);
+};
+
+export default App;
