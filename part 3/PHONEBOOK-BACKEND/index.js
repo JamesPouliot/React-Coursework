20c33757c2a8c301097cf3efccec6521841fe9a6
ITDbrookline <itdbrookline@gmail.com>
2025-09-02 17:05:36 -0400

    Phonebook exercise 3.2 complete: backend now counts the total number of people in phonebook and gives the time when the request was received.

--- part 3/PHONEBOOK-BACKEND/index.js
+++ part 3/PHONEBOOK-BACKEND/index.js
@@ -30,6 +30,12 @@ app.get('/api/persons', (request, response) => {
 	response.json(persons);
 });
 
+app.get('/api/info', (request, response) => {
+	const totalPersons = persons.length;
+	const date = new Date().toString();
+	response.send(`<p>Phonebook has info for ${totalPersons} people.</p><p>${date}</p>`);
+});
+
 const PORT = 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
