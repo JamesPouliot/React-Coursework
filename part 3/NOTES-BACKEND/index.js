f09639a2ebffc70d091fb67a641079e997fbe6a9
ITDbrookline <itdbrookline@gmail.com>
2025-09-03 16:38:23 -0400

    Finished 3.a coursework, now working on exercises, starting with 3.6. This is BEFORE starting 3.6.

--- part 3/NOTES-BACKEND/index.js
+++ part 3/NOTES-BACKEND/index.js
@@ -3,6 +3,15 @@ const app = express();
 
 app.use(express.json());
 
+const requestLogger = (request, response, next) => {
+	console.log('Method: ', request.method);
+	console.log('Path: ', request.path);
+	console.log('Body: ');
+	console.log('---');
+	next();
+};
+app.use(requestLogger);
+
 let notes = [
 	{ id: '1', content: 'HTML is easy', important: true },
 	{ id: '2', content: 'Browser can execute only JavaScript', important: false },
@@ -56,6 +65,11 @@ app.post('/api/notes', (request, response) => {
 
 	response.json(note);
 });
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({ error: 'unknown endpoint' });
+};
+app.use(unknownEndpoint);
 
 const PORT = 3001;
 app.listen(PORT);
